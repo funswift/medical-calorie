e44875aed2377ae029d0142a2dc1bd35e8a91451
i544c <leo@i544c.me>
2017-11-29 12:51:13 +0900

    カテゴリ全体を取ってこれるようにした

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from progressbar import ProgressBar, Percentage, Bar
 import json
 
-base_url = "http://calorie.slism.jp/"
+base_url = "http://calorie.slism.jp"
 
 class Calorieslism:
     def __data_to_dict(self, table):
@@ -17,7 +18,7 @@ class Calorieslism:
         @param dish_id 料理のid
         @return 栄養価の辞書
         """
-        res = requests.get(base_url + str(dish_id))
+        res = requests.get("{base}/{dish_id}".format(base=base_url, dish_id=dish_id))
         soup = BeautifulSoup(res.text, "lxml")
 
         res_data = {}
@@ -27,25 +28,42 @@ class Calorieslism:
         
         return res_data
 
-    def get_dish_list(self):
+    def get_last_page(self, page, soup):
+        """
+        最後のページかどうか
+        """
+        pager = soup.find("div", id="pager")
+        href = pager.find("a", text="Last")["href"]
+        return href.rsplit("/", 1)[1]
+
+    def get_dish_list(self, category_id, page=1):
         """
         カテゴリの食品のリストを返すよ。
         @param category_id カテゴリのid
         @return 食品の辞書
         """
-        res = requests.get(base_url + "category/16")
+        res = requests.get("{base}/category/{cat_id}/{page}".format(base=base_url, cat_id=category_id, page=page))
         soup = BeautifulSoup(res.text, "lxml")
 
         food_list =  soup.find("div", id="foodList")
         res = {}
-        for li in food_list.find_all("li"):
+        all_size = len(food_list.find_all("li"))
+        #progress = ProgressBar(widgets=[Percentage(), Bar()], maxval=100).start()
+
+        for i, li in enumerate(food_list.find_all("li")):
             dish_name = li.a.string
             dish_id = li.a["href"].strip("/")
             dish_data = self.get_dish_data(dish_id)
             res.update({dish_name: dish_data})
+            per = int((i+1) * 100 / all_size)
+            #progress.update(per)
+
+        last_page =  int(self.get_last_page(page, soup))
+        if page != last_page:
+            res.update(self.get_dish_list(category_id, page=page+1))
 
         return res
 
 #res = Calorieslism().get_dish_data(dish_id=200000)
-res = Calorieslism().get_dish_list()
+res = Calorieslism().get_dish_list(category_id=16)
 print(json.dumps(res))
